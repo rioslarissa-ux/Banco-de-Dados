5aad106b6983bf74e2c89ee32067b5fef46d22cc
rioslarissa-ux <rios.larissa@escola.pr.gov.br>
2026-03-19 23:43:34 -0300

    Create  banco-pessoal.sql

---
+++ banco-pessoal.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Funcionarios (
+    ID_Funcionario INT PRIMARY KEY,
+    Nome_Completo VARCHAR(100) NOT NULL,
+    Salario_Hora DECIMAL(10, 2) NOT NULL,
+    Observacoes_Gerais TEXT
+);
